3cc3f1bb39a0709e43c0e9e568a1f248f740530d
jtodea <jochen.todea@googlemail.com>
2013-09-30 08:42:30 +0200

    moved authn and authz classes to auth-server project

--- resource-server/src/main/java/org/osiam/security/authentication/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * org.osiam.security.authentication is a group of classes which are needed by spring-security to authenticate an User
- * and clients.
- *
- */
-package org.osiam.security.authentication;
--- resource-server/src/main/java/org/osiam/security/controller/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * org.osiam.security.controller is a group of classes which do generate the http api on deployment for security
- * related purposes.
- */
-package org.osiam.security.controller;
