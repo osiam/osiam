71ef10c14f21ddb5a3bd715974e13951e9e8787d
jtodea <jochen.todea@googlemail.com>
2013-10-17 17:01:53 +0200

    fixed sonar violations

--- resource-server/src/main/java/org/osiam/storage/dao/GetInternalIdSkeleton.java
+++ resource-server/src/main/java/org/osiam/storage/dao/GetInternalIdSkeleton.java
@@ -41,7 +41,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import java.util.List;
-import java.util.UUID;
 import java.util.logging.Logger;
 
 public abstract class GetInternalIdSkeleton {
