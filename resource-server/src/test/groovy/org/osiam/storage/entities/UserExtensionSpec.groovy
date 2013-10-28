92725ef9f7891166b0dda86b1968b690eb973cb0
Thomas Krille <t.krille@tarent.de>
2013-10-28 13:24:11 +0100

    [test] added test for updating extension value

--- resource-server/src/test/groovy/org/osiam/storage/entities/UserExtensionSpec.groovy
+++ resource-server/src/test/groovy/org/osiam/storage/entities/UserExtensionSpec.groovy
@@ -1,19 +1,22 @@
 package org.osiam.storage.entities
 
+import org.codehaus.groovy.ast.stmt.ContinueStatement;
 import org.osiam.resources.scim.Extension
 import org.osiam.resources.scim.User
 import org.osiam.storage.entities.extension.ExtensionEntity
+import org.osiam.storage.entities.extension.ExtensionFieldEntity
 import org.osiam.storage.entities.extension.ExtensionFieldValueEntity
 
 import spock.lang.Specification
 
 class UserExtensionSpec extends Specification {
-    
+
     UserEntity userEntity = new UserEntity()
-    
+
     def "adding extensions to a user should result in setting the user also to the extension"(){
         given:
-        def extensions = [new ExtensionFieldValueEntity()] as Set
+        def extensions = [
+            new ExtensionFieldValueEntity()] as Set
         userEntity.setUserExtensions(extensions)
 
         when:
@@ -67,55 +70,86 @@ class UserExtensionSpec extends Specification {
         sortedExtensionList[3].getExtensionField().getName() == "size"
         sortedExtensionList[3].getExtensionField().getExtension().getExtensionUrn() == "urn2"
     }
-    
+
     def 'extensions can be registered'() {
         given:
         def extension = new ExtensionEntity()
-        
+
         when:
         userEntity.registerExtension(extension);
-        
+
         then:
         userEntity.registeredExtensions.contains(extension)
     }
-    
+
     def 'registering null as extension raises exception'() {
         when:
         userEntity.registerExtension(null);
-        
+
         then:
         thrown(IllegalArgumentException)
     }
-    
+
     def 'adding a field value works'() {
         given:
         def extension = new ExtensionEntity()
         def fieldValue = new ExtensionFieldValueEntity()
-        
+
         when:
         userEntity.addOrUpdateExtensionValue(extension, fieldValue)
-        
+
         then:
         userEntity.extensionFieldValues.contains(fieldValue)
     }
-    
+
     def 'adding null as extension value raises exception'() {
         given:
         def extension = new ExtensionEntity()
-        
+
         when:
         userEntity.addOrUpdateExtensionValue(extension, null)
-        
+
         then:
         thrown(IllegalArgumentException)
     }
-    
+
     def 'updating an extension value works'() {
         given:
         def extension = new ExtensionEntity()
+        def extensionField = new ExtensionFieldEntity()
+        extensionField.extension = extension
+        extensionField.name = "testField"
         def fieldValue = new ExtensionFieldValueEntity()
+        fieldValue.extensionField = extensionField
+        fieldValue.value = "fieldValue"
         def fieldValueUpdated = new ExtensionFieldValueEntity()
+        fieldValueUpdated.extensionField = extensionField
+        fieldValueUpdated.value = "fieldValueUpdated"
+        userEntity.addOrUpdateExtensionValue(extension, fieldValue)
+
+        when:
+        userEntity.addOrUpdateExtensionValue(extension, fieldValueUpdated)
+
+        then:
         
-        
+        extensionValuesOnlyContains(fieldValueUpdated, extensionField) == true
+    }
+    
+    def extensionValuesOnlyContains(fieldValue, extensionField) {
+        def ok = false;
+        for (extensionFieldValue in userEntity.extensionFieldValues) {
+            if(extensionFieldValue.extensionField != extensionField) {
+                continue
+            }
+            
+            if (extensionFieldValue.value == fieldValue.value) {
+                ok = true
+                continue
+            } else {
+                ok = false
+                break
+            }
+        }
+        ok
     }
 }
