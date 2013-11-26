b6252e7a092f7cfa38ff62286c840c6dbc02e2d3
Thomas Krille <tkrille@gmail.com>
2013-11-26 16:38:33 +0100

    [fix] unignored and fixed tests

--- resource-server/src/test/groovy/org/osiam/resources/provisioning/ScimUserProvisioningBeanSpec.groovy
+++ resource-server/src/test/groovy/org/osiam/resources/provisioning/ScimUserProvisioningBeanSpec.groovy
@@ -2,12 +2,15 @@ package org.osiam.resources.provisioning
 
 import org.osiam.resources.converter.UserConverter
 import org.osiam.resources.exceptions.ResourceExistsException
+import org.osiam.resources.scim.Meta
 import org.osiam.resources.scim.SCIMSearchResult
 import org.osiam.resources.scim.User
+import org.osiam.storage.dao.SearchResult
 import org.osiam.storage.dao.UserDao
 import org.osiam.storage.entities.MetaEntity
 import org.osiam.storage.entities.UserEntity
 import org.springframework.security.authentication.encoding.PasswordEncoder
+
 import spock.lang.Ignore
 import spock.lang.Specification
 
@@ -17,10 +20,8 @@ class ScimUserProvisioningBeanSpec extends Specification {
     UserDao userDao = Mock()
     UserConverter userConverter = Mock()
 
-
     SCIMUserProvisioningBean scimUserProvisioningBean = new SCIMUserProvisioningBean(userDao: userDao,
-            userConverter: userConverter,
-            passwordEncoder: passwordEncoder)
+    userConverter: userConverter, passwordEncoder: passwordEncoder)
 
     def 'should be possible to get an user by his id'() {
 
@@ -43,57 +44,71 @@ class ScimUserProvisioningBeanSpec extends Specification {
         1 * passwordEncoder.encodePassword(_, _) >> "password"
         1 * userConverter.fromScim(_) >> new UserEntity(userName: 'test')
         1 * userDao.create(_)
-        1 * userConverter.toScim(_) >> { UserEntity it -> new User.Builder(id: it.id).build() }
+        1 * userConverter.toScim(_) >> { UserEntity it ->
+            new User.Builder(id: it.id).build()
+        }
         user.id ==~ '[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}'
     }
 
-    @Ignore('Temporarily ignored because of merge in propgress')
     def 'should throw exception if user name already exists'() {
         given:
-        def exception = Mock(Exception)
-        userDao.create(_) >> { throw exception }
-        scimUser.getUserName() >> 'test'
-        exception.getMessage() >> 'scim_user_username_key'
+        def userName = 'irrelevant'
+        def userEntity = new UserEntity()
+        def userScim = new User(userName: userName)
 
         when:
-        scimUserProvisioningBean.create(scimUser)
+        scimUserProvisioningBean.create(userScim)
 
         then:
-        1 * userConverter.createFromScim(_) >> new UserEntity(userName: 'test')
+        1 * userConverter.fromScim(userScim) >> userEntity
+        1 * userDao.create(userEntity) >> { throw new Exception('scim_user_username_key') }
         def e = thrown(ResourceExistsException)
-        e.getMessage() == 'The user with name test already exists.'
+        e.getMessage().contains(userName)
     }
 
-    @Ignore('Temporarily ignored because of merge in propgress')
     def 'should throw exception if externalId already exists'() {
         given:
-        def exception = Mock(Exception)
-        userDao.create(_) >> { throw exception }
-        scimUser.getExternalId() >> 'irrelevant'
-        exception.getMessage() >> 'scim_id_externalid_key'
+        def externalId = 'irrelevant'
+        def userEntity = new UserEntity()
+        def userScim = new User.Builder().setExternalId(externalId).build()
 
         when:
-        scimUserProvisioningBean.create(scimUser)
+        scimUserProvisioningBean.create(userScim)
 
         then:
-        1 * userConverter.createFromScim(_) >> new UserEntity()
+        1 * userConverter.fromScim(userScim) >> userEntity
+        1 * userDao.create(userEntity) >> { throw new Exception('scim_id_externalid_key') }
         def e = thrown(ResourceExistsException)
-        e.getMessage() == 'The user with the externalId irrelevant already exists.'
+        e.getMessage().contains(externalId)
     }
 
-    @Ignore('Temporarily ignored because of merge in propgress')
-    def 'should get an user before update, set the expected fields, merge the result'() {
+    def 'should get a user before replace, set the unmodifiable fields, merge the result and return scim user'() {
         given:
-        def internalId = UUID.randomUUID()
-        def scimUser = new User.Builder('test').build()
-        def entity = new UserEntity(userName: 'username')
-        entity.setId(internalId)
+        def id = UUID.randomUUID()
+        def idString = id.toString()
+        def internalId = 1L
+        def meta = new MetaEntity()
+        def userScim = new User()
+
+        UserEntity existingEntity = Mock()
+        UserEntity userEntity = Mock()
 
         when:
-        scimUserProvisioningBean.replace(internalId.toString(), scimUser)
+        scimUserProvisioningBean.replace(idString, userScim)
+
         then:
-        1 * userConverter.createFromScim(scimUser, internalId.toString()) >> entity
-        1 * userDao.update(entity) >> entity
+        1 * userDao.getById(idString) >> existingEntity
+        1 * userConverter.fromScim(userScim) >> userEntity
+
+        1 * existingEntity.getInternalId() >>internalId
+        1 * existingEntity.getMeta() >> meta
+        1 * existingEntity.getId() >> id
+        1 * userEntity.setInternalId(internalId)
+        1 * userEntity.setMeta(meta)
+        1 * userEntity.setId(id)
+
+        1 * userDao.update(userEntity) >> userEntity
+        1 * userConverter.toScim(userEntity)
     }
 
     def 'should wrap IllegalAccessException to an IllegalState'() {
@@ -101,12 +116,11 @@ class ScimUserProvisioningBeanSpec extends Specification {
         GenericSCIMToEntityWrapper setUserFields = Mock(GenericSCIMToEntityWrapper)
 
         when:
-        scimUserProvisioningBean.setFieldsWrapException(setUserFields);
+        scimUserProvisioningBean.setFieldsWrapException(setUserFields)
         then:
         1 * setUserFields.setFields() >> { throw new IllegalAccessException('Blubb') }
         def e = thrown(IllegalStateException)
         e.message == 'This should not happen.'
-
     }
 
     def 'should call dao delete on delete'() {
@@ -118,24 +132,22 @@ class ScimUserProvisioningBeanSpec extends Specification {
         1 * userDao.delete(id)
     }
 
-    @Ignore('Don\'t mock the EntityManager or result here')
     def 'should call dao search on search'() {
         given:
-        def scimSearchResultMock = Mock(SCIMSearchResult)
-        userDao.search('anyFilter', 'userName', 'ascending', 100, 1) >> scimSearchResultMock
-
-        UserEntity userEntity = Mock()
-        userEntity.getUserName() >> 'someName'
-        userEntity.getId() >> UUID.randomUUID()
-        userEntity.getMeta() >> new MetaEntity()
-        def userList = [userEntity] as List
-        scimSearchResultMock.getResources() >> userList
-        scimSearchResultMock.getTotalResults() >> 1000.toLong()
+        UserEntity userEntity = new UserEntity()
+        User userScim = new User()
+        SearchResult searchResult = new SearchResult([userEntity] as List, 1000L)
+        userDao.search('anyFilter', 'userName', 'ascending', 100, 1) >> searchResult
 
         when:
         def result = scimUserProvisioningBean.search('anyFilter', 'userName', 'ascending', 100, 1)
 
         then:
-        result.getTotalResults() == 1000.toLong()
+        1 * userDao.search('anyFilter', 'userName', 'ascending', 100, 0) >> searchResult
+        1 * userConverter.toScim(userEntity) >> userScim
+        result.getResources().size() == 1
+        result.getStartIndex() == 1
+        result.getItemsPerPage() == 100
+        result.getTotalResults() == 1000L
     }
 }
