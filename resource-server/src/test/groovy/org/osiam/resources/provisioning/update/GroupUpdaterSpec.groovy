a761a2921e756270da3cd70496d5817ce00018cb
Thomas Krille <tkrille@gmail.com>
2013-12-18 10:15:42 +0100

    [enhancement] tightened specs and added descriptions

--- resource-server/src/test/groovy/org/osiam/resources/provisioning/update/GroupUpdaterSpec.groovy
+++ resource-server/src/test/groovy/org/osiam/resources/provisioning/update/GroupUpdaterSpec.groovy
@@ -38,11 +38,11 @@ class GroupUpdaterSpec extends Specification {
         when:
         groupUpdater.update(group, groupEntity)
 
-        then:
-        0 * groupEntity.setDisplayName(_)
+        then: 'the entity was not changed'
+        0 * groupEntity._
     }
 
-    def 'updating displayName works'() {
+    def 'updating displayName is possible'() {
         given:
         def displayName = 'irrelevant'
         group = new Group.Builder(displayName: displayName).build()
@@ -50,8 +50,10 @@ class GroupUpdaterSpec extends Specification {
         when:
         groupUpdater.update(group, groupEntity)
 
-        then:
+        then: 'only the displayName was changed'
         1 * groupEntity.setDisplayName(displayName)
+        and: 'nothing else was changed'
+        0 * groupEntity._
     }
 
     @Unroll
@@ -62,8 +64,8 @@ class GroupUpdaterSpec extends Specification {
         when:
         groupUpdater.update(group, groupEntity)
 
-        then:
-        0 * groupEntity.setDisplayName(_)
+        then: 'the entity was not changed'
+        0 * groupEntity._
 
         where:
         value          | displayName
@@ -71,7 +73,7 @@ class GroupUpdaterSpec extends Specification {
         'empty string' | ''
     }
 
-    def 'removing all members works'() {
+    def 'removing all members is possible'() {
         given:
         Meta meta = new Meta(attributes: ['members'] as Set)
         group = new Group.Builder(meta: meta).build()
@@ -79,11 +81,13 @@ class GroupUpdaterSpec extends Specification {
         when:
         groupUpdater.update(group, groupEntity)
 
-        then:
+        then: 'all members were removed'
         1 * groupEntity.removeAllMembers()
+        and: 'nothing else was changed'
+        0 * groupEntity._
     }
 
-    def 'adding a member works'(){
+    def 'adding a member is possible'(){
         given:
         def memberId = 'irrelevant'
         def member = new GroupEntity()
@@ -94,12 +98,15 @@ class GroupUpdaterSpec extends Specification {
         when:
         groupUpdater.update(group, groupEntity)
 
-        then:
+        then: 'the member was retrieved via the ResourceDao'
         1 * resourceDao.getById(memberId, ResourceEntity) >> member
+        and: 'the retrieved member was added to the group'
         1 * groupEntity.addMember(member)
+        and: 'nothing else was changed'
+        0 * groupEntity._
     }
 
-    def 'removing a member works'(){
+    def 'removing a member is possible'(){
         given:
         def memberId = UUID.randomUUID()
         GroupEntity member = new GroupEntity(id: memberId)
@@ -110,8 +117,11 @@ class GroupUpdaterSpec extends Specification {
         when:
         groupUpdater.update(group, groupEntity)
 
-        then:
+        then: 'the member to remove was looked up in the current set of members of the group'
         1 * groupEntity.getMembers() >> ([member] as Set)
+        and: 'the looked up member was removed'
         1 * groupEntity.removeMember(member)
+        and: 'nothing else was changed'
+        0 * groupEntity._
     }
 }
