bbaf19e1a6128f7c2486004258fb61918a0f70e5
Thomas Krille <tkrille@gmail.com>
2013-12-18 15:39:19 +0100

    [fix] removed useless tests for setters and getters

--- resource-server/src/test/groovy/org/osiam/storage/entities/AddressEntitySpec.groovy
+++ resource-server/src/test/groovy/org/osiam/storage/entities/AddressEntitySpec.groovy
@@ -30,92 +30,12 @@ class AddressEntitySpec extends Specification {
     AddressEntity addressEntity = new AddressEntity()
     def userEntity = Mock(UserEntity)
 
-    def "setter and getter for the Id should be present"() {
-        when:
-        addressEntity.setId(123456)
-
-        then:
-        addressEntity.getId() == 123456
-    }
-
-    def "setter and getter for the formatted address should be present"() {
-        when:
-        addressEntity.setFormatted("funky street 1337 Honkytown")
-
-        then:
-        addressEntity.getFormatted() == "funky street 1337 Honkytown"
-    }
-
-    def "setter and getter for the street address should be present"() {
-        when:
-        addressEntity.setStreetAddress("funky street")
-
-        then:
-        addressEntity.getStreetAddress() == "funky street"
-    }
-
-    def "setter and getter for the locality should be present"() {
-        when:
-        addressEntity.setLocality("locality")
-
-        then:
-        addressEntity.getLocality() == "locality"
-    }
-
-    def "setter and getter for the region should be present"() {
-        when:
-        addressEntity.setRegion("region")
-
-        then:
-        addressEntity.getRegion() == "region"
-    }
-
-    def "setter and getter for the postal code should be present"() {
-        when:
-        addressEntity.setPostalCode("123456")
-
-        then:
-        addressEntity.getPostalCode() == "123456"
-    }
-
-    def "setter and getter for the country should be present"() {
-        when:
-        addressEntity.setCountry("Germany")
-
-        then:
-        addressEntity.getCountry() == "Germany"
-    }
-
-    def "setter and getter for the type should be present"() {
-        when:
-        addressEntity.setType("home")
-
-        then:
-        addressEntity.getType() == "home"
-    }
-
     def "getter for the type should not throw exception if type is null"() {
         when:
         addressEntity.setType(null)
 
         then:
         addressEntity.getType() == null
-    }
-
-    def "setter and getter for primary should be present"() {
-        when:
-        addressEntity.setPrimary(true)
-
-        then:
-        addressEntity.isPrimary()
-    }
-
-    def "setter and getter for the user should be present"() {
-        when:
-        addressEntity.setUser(userEntity)
-
-        then:
-        addressEntity.getUser() == userEntity
     }
 
 }
