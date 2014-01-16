bd3e9f203e18b2ce47083cad08d63efcd30f4e4d
Thomas Krille <tkrille@gmail.com>
2014-01-16 17:18:13 +0100

    [fix] changed sql scripts to pin down table field order

--- resource-server/src/main/resources/sql/example_data.sql
+++ resource-server/src/main/resources/sql/example_data.sql
@@ -2,42 +2,43 @@
 -- Updating hibernate sequence start value
 --
 SELECT
-  pg_catalog.setval('hibernate_sequence', 4, FALSE);
-
+  pg_catalog.setval('hibernate_sequence', 5, FALSE);
 
 --
--- Default data for user, role, meta and client
+-- Example Client
 --
-INSERT INTO scim_meta(id, created, lastmodified, location, resourcetype, version)
-    VALUES (2, '2011-10-10', '2011-10-10', NULL, 'User', NULL);
-
-INSERT INTO scim_id(internal_id, externalid, id, meta_id)
-    VALUES (1, 2, 'cef9452e-00a9-4cec-a086-d171374ffbef', NULL);
-
-INSERT INTO osiam_client(internal_id, accesstokenvalidityseconds, client_secret, expiry, 
+INSERT INTO osiam_client (internal_id, accesstokenvalidityseconds, client_secret, expiry, 
             id, implicit_approval, redirect_uri, refreshtokenvalidityseconds, 
             validityinseconds)
-    VALUES (3, 2342, 'secret', null, 
+    VALUES (1, 2342, 'secret', null, 
             'example-client', FALSE, 'http://localhost:5000/oauth2', 4684,
             1337);
-INSERT INTO osiam_client_scopes VALUES (3, 'GET');
-INSERT INTO osiam_client_scopes VALUES (3, 'POST');
-INSERT INTO osiam_client_scopes VALUES (3, 'PUT');
-INSERT INTO osiam_client_scopes VALUES (3, 'PATCH');
-INSERT INTO osiam_client_scopes VALUES (3, 'DELETE');
+INSERT INTO osiam_client_scopes (id, scope) VALUES (1, 'GET');
+INSERT INTO osiam_client_scopes (id, scope) VALUES (1, 'POST');
+INSERT INTO osiam_client_scopes (id, scope) VALUES (1, 'PUT');
+INSERT INTO osiam_client_scopes (id, scope) VALUES (1, 'PATCH');
+INSERT INTO osiam_client_scopes (id, scope) VALUES (1, 'DELETE');
 
-INSERT INTO osiam_client_grants VALUES (3, 'authorization_code');
-INSERT INTO osiam_client_grants VALUES (3, 'refresh_token');
-INSERT INTO osiam_client_grants VALUES (3, 'password');
-INSERT INTO osiam_client_grants VALUES (3, 'client_credentials');
+INSERT INTO osiam_client_grants (id, grants) VALUES (1, 'authorization_code');
+INSERT INTO osiam_client_grants (id, grants) VALUES (1, 'refresh_token');
+INSERT INTO osiam_client_grants (id, grants) VALUES (1, 'password');
+INSERT INTO osiam_client_grants (id, grants) VALUES (1, 'client_credentials');
 
-INSERT INTO scim_user(active, displayname, locale, nickname, password, preferredlanguage, 
+--
+-- Example User: marissa, pw: koala, Roles: 'USER'
+--
+INSERT INTO scim_meta (id, created, lastmodified, location, resourcetype, version)
+    VALUES (2, '2011-10-10', '2011-10-10', NULL, 'User', NULL);
+
+INSERT INTO scim_id (internal_id, externalid, id, meta_id)
+    VALUES (3, NULL, 'cef9452e-00a9-4cec-a086-d171374ffbef', 2);
+
+INSERT INTO scim_user (active, displayname, locale, nickname, password, preferredlanguage, 
             profileurl, timezone, title, username, usertype, internal_id, 
             name_id)
     VALUES (TRUE, NULL, NULL, NULL, 'cbae73fac0893291c4792ef19d158a589402288b35cb18fb8406e951b9d95f6b8b06a3526ffebe96ae0d91c04ae615a7fe2af362763db386ccbf3b55c29ae800', NULL, 
-            NULL, NULL, NULL, 'marissa', NULL, 1, 
+            NULL, NULL, NULL, 'marissa', NULL, 3, 
             NULL);
 
-INSERT INTO scim_roles(
-            multi_value_id, value, user_internal_id)
-    VALUES (1, 'USER', 1);
+INSERT INTO scim_roles (multi_value_id, value, user_internal_id)
+    VALUES (4, 'USER', 3);
