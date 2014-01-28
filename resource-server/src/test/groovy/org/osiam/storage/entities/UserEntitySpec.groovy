856c6d33daa55692e29c93a5cdcd765b32df5eef
David Moebius <dmoebius@gmail.com>
2014-01-28 13:36:46 +0100

    [refactoring] removed unused lines of codes

--- resource-server/src/test/groovy/org/osiam/storage/entities/UserEntitySpec.groovy
+++ resource-server/src/test/groovy/org/osiam/storage/entities/UserEntitySpec.groovy
@@ -23,17 +23,6 @@
 
 package org.osiam.storage.entities
 
-import org.osiam.resources.scim.Address
-import org.osiam.resources.scim.Email
-import org.osiam.resources.scim.Entitlement
-import org.osiam.resources.scim.GroupRef
-import org.osiam.resources.scim.Im
-import org.osiam.resources.scim.Name
-import org.osiam.resources.scim.PhoneNumber
-import org.osiam.resources.scim.Photo
-import org.osiam.resources.scim.Role
-import org.osiam.resources.scim.X509Certificate
-
 import spock.lang.Specification
 
 class UserEntitySpec extends Specification {
@@ -41,87 +30,6 @@ class UserEntitySpec extends Specification {
     private static final VALUE = 'irrelevant'
 
     UserEntity userEntity = new UserEntity()
-
-    def name = new NameEntity()
-    def scimName
-
-    def addresses = new ArrayList()
-    def emails = new ArrayList()
-    def entitlements = new ArrayList()
-    def groups = new ArrayList()
-    def ims = new ArrayList()
-    def phoneNumbers = new ArrayList()
-    def photos = new ArrayList()
-    def roles = new ArrayList()
-    def certificates = new ArrayList()
-
-    def setup() {
-        name.setFamilyName("familyName")
-        name.setFormatted("formattedName")
-        name.setGivenName("givenName")
-        name.setHonorificPrefix("prefix")
-        name.setHonorificSuffix("suffix")
-        name.setId(1234)
-        name.setMiddleName("middleName")
-
-
-        scimName = new Name.Builder().
-                setFamilyName("familyName").
-                setFormatted("formattedName").
-                setGivenName("givenName").
-                setHonorificPrefix("prefix").
-                setHonorificSuffix("suffix").
-                setMiddleName("middleName").
-                build()
-
-        addresses.add(new Address.Builder().
-                setCountry("country").
-                setFormatted("formatted").
-                setLocality("locality").
-                setPostalCode("123456").
-                setRegion("region").
-                setStreetAddress("streetAddress")
-                .setPrimary(true).
-                build())
-
-        emails.add(new Email.Builder().
-                setPrimary(true).
-                setType(Email.Type.HOME).
-                setValue(VALUE).
-                build())
-
-        entitlements.add(new Entitlement.Builder().
-                setValue(VALUE).
-                build())
-
-        groups.add(new GroupRef.Builder().
-                setValue(UUID.randomUUID().toString()).
-                setDisplay("display").
-                build())
-
-        ims.add(new Im.Builder().
-                setValue(VALUE).
-                setType(Im.Type.ICQ).
-                build())
-
-        phoneNumbers.add(new PhoneNumber.Builder().
-                setValue(VALUE).
-                setType(PhoneNumber.Type.HOME).
-                build())
-
-        photos.add(new Photo.Builder().
-                setValue("picture.gif").
-                setType(Photo.Type.PHOTO).
-                build())
-
-        roles.add(new Role.Builder().
-                setValue("").
-                build())
-
-        certificates.add(new X509Certificate.Builder().
-                setValue(VALUE).
-                build())
-    }
 
     def "should be able to insert meta"() {
         given:
@@ -158,5 +66,4 @@ class UserEntitySpec extends Specification {
         then:
         emptySet != null
     }
-
 }
