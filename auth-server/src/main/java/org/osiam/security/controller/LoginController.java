bbc4035b6870b042d84318ccaeca0b8b866dd5f9
David Moebius <dmoebius@gmail.com>
2014-03-18 16:02:51 +0100

    [task] added thymeleafe and new loginpage

---
+++ auth-server/src/main/java/org/osiam/security/controller/LoginController.java
@@ -0,0 +1,14 @@
+package org.osiam.security.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping("/login")
+    public String login() {
+        return "login";
+    }
+    
+}
