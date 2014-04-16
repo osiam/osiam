c2c87bf7a51ed3e50f0615f1410bcf971552b0da
David Moebius <dmoebius@gmail.com>
2014-04-16 14:47:30 +0200

    [task] added auth extension and field origin. Saving ldap in extension in case the user comes from ldap

--- resource-server/src/main/resources/sql/init_data.sql
+++ resource-server/src/main/resources/sql/init_data.sql
@@ -19,3 +19,8 @@ INSERT INTO osiam_client_scopes (id, scope) VALUES (2, 'POST');
 INSERT INTO osiam_client_scopes (id, scope) VALUES (2, 'PATCH');
 
 INSERT INTO osiam_client_grants (id, grants) VALUES (2, 'client_credentials');
+
+INSERT INTO scim_extension VALUES (1, 'urn:scim:schemas:osiam:2.0:authentication:server');
+
+INSERT INTO scim_extension_field (internal_id, is_required, name, type, extension_internal_id)
+	VALUES (1, false, 'origin', 'STRING', 1);
