994c9ffab4c9ca05a11fa9395ffd2f276f6226ee
David Crome <d.crome@tarent.de>
2014-04-28 16:08:45 +0200

    [fix] access token wont be created before client created

--- auth-server/src/main/java/org/osiam/auth/oauth_client/OsiamAuthServerClientProvider.java
+++ auth-server/src/main/java/org/osiam/auth/oauth_client/OsiamAuthServerClientProvider.java
@@ -14,6 +14,10 @@ import org.osiam.client.oauth.GrantType;
 import org.osiam.client.oauth.Scope;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
 
 @Service
 public class OsiamAuthServerClientProvider {
@@ -21,6 +25,9 @@ public class OsiamAuthServerClientProvider {
     private static final Logger LOGGER = Logger.getLogger(OsiamAuthServerClientProvider.class.getName());
     
     public static final String AUTH_SERVER_CLIENT_ID = "auth-server";
+    
+    @Inject
+    private PlatformTransactionManager txManager;
     
     @Inject
     private ClientDao clientDao;
@@ -32,43 +39,48 @@ public class OsiamAuthServerClientProvider {
     
     @PostConstruct
     private void createAuthServerClient() {
-        
-        ClientEntity clientEntity = null;
-        
-        try {
-            clientEntity = clientDao.getClient(AUTH_SERVER_CLIENT_ID);
-        } catch (ResourceNotFoundException e) {
-            LOGGER.log(Level.INFO, "No auth server found. The auth server will be created.");
-        }
-        
-        if(clientEntity == null) {
-            clientEntity = new ClientEntity();
-            Set<String> scopes = new HashSet<String>();
-            scopes.add(Scope.GET.toString());
-            scopes.add(Scope.POST.toString());
-            scopes.add(Scope.PUT.toString());
-            scopes.add(Scope.PATCH.toString());
-            scopes.add(Scope.DELETE.toString());
+        TransactionTemplate tmpl = new TransactionTemplate(txManager);
+        tmpl.execute(new TransactionCallbackWithoutResult() {
+            @Override
+            protected void doInTransactionWithoutResult(TransactionStatus status) {
+                ClientEntity clientEntity = null;
+                
+                try {
+                    clientEntity = clientDao.getClient(AUTH_SERVER_CLIENT_ID);
+                } catch (ResourceNotFoundException e) {
+                    LOGGER.log(Level.INFO, "No auth server found. The auth server will be created.");
+                }
+                
+                if(clientEntity == null) {
+                    clientEntity = new ClientEntity();
+                    Set<String> scopes = new HashSet<String>();
+                    scopes.add(Scope.GET.toString());
+                    scopes.add(Scope.POST.toString());
+                    scopes.add(Scope.PUT.toString());
+                    scopes.add(Scope.PATCH.toString());
+                    scopes.add(Scope.DELETE.toString());
+                    
+                    Set<String> grants = new HashSet<String>();
+                    grants.add(GrantType.CLIENT_CREDENTIALS.toString());
+                    
+                    clientEntity.setId(AUTH_SERVER_CLIENT_ID);
+                    clientEntity.setRefreshTokenValiditySeconds(3600);
+                    clientEntity.setAccessTokenValiditySeconds(3600);
+                    clientEntity.setRedirectUri(authServerHome);
+                    clientEntity.setScope(scopes);
+                    Calendar calendar = Calendar.getInstance();
+                    calendar.set(Calendar.YEAR, 3000);
+                    clientEntity.setExpiry(calendar.getTime());
+                    clientEntity.setImplicit(true);
+                    clientEntity.setValidityInSeconds(3600);
+                    clientEntity.setGrants(grants);
             
-            Set<String> grants = new HashSet<String>();
-            grants.add(GrantType.CLIENT_CREDENTIALS.toString());
-            
-            clientEntity.setId(AUTH_SERVER_CLIENT_ID);
-            clientEntity.setRefreshTokenValiditySeconds(3600);
-            clientEntity.setAccessTokenValiditySeconds(3600);
-            clientEntity.setRedirectUri(authServerHome);
-            clientEntity.setScope(scopes);
-            Calendar calendar = Calendar.getInstance();
-            calendar.set(Calendar.YEAR, 3000);
-            clientEntity.setExpiry(calendar.getTime());
-            clientEntity.setImplicit(true);
-            clientEntity.setValidityInSeconds(3600);
-            clientEntity.setGrants(grants);
-    
-            clientEntity = clientDao.create(clientEntity);
-        }
-        
-        clientSecret = clientEntity.getClientSecret();
+                    clientEntity = clientDao.create(clientEntity);
+                }
+                
+                clientSecret = clientEntity.getClientSecret();
+            }
+        });
     }
 
     public String getClientSecret() {
--- auth-server/src/main/java/org/osiam/auth/token/OsiamAccessTokenProvider.java
+++ auth-server/src/main/java/org/osiam/auth/token/OsiamAccessTokenProvider.java
@@ -9,16 +9,14 @@ import javax.inject.Inject;
 import org.osiam.auth.oauth_client.OsiamAuthServerClientProvider;
 import org.osiam.client.oauth.AccessToken;
 import org.osiam.client.oauth.Scope;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.security.oauth2.provider.DefaultAuthorizationRequest;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.TransactionCallbackWithoutResult;
-import org.springframework.transaction.support.TransactionTemplate;
 
 @Service
+@DependsOn("osiamAuthServerClientProvider")
 public class OsiamAccessTokenProvider {
 
     private AccessToken accessToken;
@@ -26,30 +24,21 @@ public class OsiamAccessTokenProvider {
     @Inject
     private DefaultTokenServices tokenServices;
     
-    @Inject
-    private PlatformTransactionManager txManager;
-    
     @PostConstruct
     public void createAccessToken() {
-        TransactionTemplate tmpl = new TransactionTemplate(txManager);
-        tmpl.execute(new TransactionCallbackWithoutResult() {
-            @Override
-            protected void doInTransactionWithoutResult(TransactionStatus status) {
-                Set<String> scopes = new HashSet<String>();
-                scopes.add(Scope.GET.toString());
-                scopes.add(Scope.POST.toString());
-                scopes.add(Scope.PUT.toString());
-                scopes.add(Scope.PATCH.toString());
-                scopes.add(Scope.DELETE.toString());
+        Set<String> scopes = new HashSet<String>();
+        scopes.add(Scope.GET.toString());
+        scopes.add(Scope.POST.toString());
+        scopes.add(Scope.PUT.toString());
+        scopes.add(Scope.PATCH.toString());
+        scopes.add(Scope.DELETE.toString());
 
-                DefaultAuthorizationRequest authorizationRequest = new DefaultAuthorizationRequest(OsiamAuthServerClientProvider.AUTH_SERVER_CLIENT_ID, scopes);
-                authorizationRequest.setApproved(true);
-                
-                OAuth2Authentication oAuth2Authentication = new OAuth2Authentication(authorizationRequest, null);
+        DefaultAuthorizationRequest authorizationRequest = new DefaultAuthorizationRequest(OsiamAuthServerClientProvider.AUTH_SERVER_CLIENT_ID, scopes);
+        authorizationRequest.setApproved(true);
+        
+        OAuth2Authentication oAuth2Authentication = new OAuth2Authentication(authorizationRequest, null);
 
-                accessToken = AccessToken.of(tokenServices.createAccessToken(oAuth2Authentication).getValue());
-            }
-        });
+        accessToken = AccessToken.of(tokenServices.createAccessToken(oAuth2Authentication).getValue());
     }
     
     public AccessToken getAccessToken() {
