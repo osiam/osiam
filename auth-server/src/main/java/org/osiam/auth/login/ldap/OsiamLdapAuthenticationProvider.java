20fe6e74f26bc6a26f28bb86679259ba31a59b62
Thomas Krille <tkrille@gmail.com>
2015-06-02 10:17:35 +0200

    [cleanup] remove unused imports

--- auth-server/src/main/java/org/osiam/auth/login/ldap/OsiamLdapAuthenticationProvider.java
+++ auth-server/src/main/java/org/osiam/auth/login/ldap/OsiamLdapAuthenticationProvider.java
@@ -43,7 +43,6 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.ldap.authentication.LdapAuthenticationProvider;
 import org.springframework.security.ldap.authentication.LdapAuthenticator;
 import org.springframework.security.ldap.userdetails.LdapAuthoritiesPopulator;
