ade01164d8e6f00c3e369213c925dd644e62ec84
Thomas Krille <t.krille@tarent.de>
2015-08-16 00:21:17 +0200

    Remove only known clients' scopes on migration
    Makes the migration more stable and trustworthy by only changing clients
    that are well-knonw to OSIAM, e.g. `auth-server`,
    `addon-self-administration`. All user-defined clients have to be
    manually migrated like described in the migration notes.

--- auth-server/src/main/java/db/migration/V4__replace_old_scopes.java
+++ auth-server/src/main/java/db/migration/V4__replace_old_scopes.java
@@ -4,13 +4,16 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.flywaydb.core.api.migration.MigrationChecksumProvider;
 import org.flywaydb.core.api.migration.jdbc.JdbcMigration;
 
 public class V4__replace_old_scopes implements JdbcMigration, MigrationChecksumProvider {
+
+    private final Map<String, Long> internalIds = new HashMap<>();
+    private Connection connection;
 
     @Override
     public Integer getChecksum() {
@@ -19,70 +22,62 @@ public class V4__replace_old_scopes implements JdbcMigration, MigrationChecksumP
 
     @Override
     public void migrate(Connection connection) throws Exception {
-        List<Long> clientIds = clientIds(connection);
-        for (Long clientId : clientIds) {
-            removeScopeFromClient(clientId, "GET", connection);
-            removeScopeFromClient(clientId, "POST", connection);
-            removeScopeFromClient(clientId, "PUT", connection);
-            removeScopeFromClient(clientId, "PATCH", connection);
-            removeScopeFromClient(clientId, "DELETE", connection);
-
-            addScopeToClient(clientId, "ADMIN", connection);
-            addScopeToClient(clientId, "ME", connection);
-        }
-
-        removeScopeFromClient(toInternalId("auth-server", connection), "ME", connection);
-        removeScopeFromClient(toInternalId("addon-administration-client", connection), "ME", connection);
-        removeScopeFromClient(toInternalId("addon-self-administration-client", connection), "ME", connection);
+        this.connection = connection;
+        removeScopesFromClient("example-client", "GET", "POST", "PUT", "PATCH", "DELETE");
+        addScopesToClient("example-client", "ADMIN", "ME");
+        removeScopesFromClient("auth-server", "GET", "POST", "PUT", "PATCH", "DELETE");
+        addScopesToClient("auth-server", "ADMIN");
+        removeScopesFromClient("addon-self-administration-client", "GET", "POST", "PUT", "PATCH", "DELETE");
+        addScopesToClient("addon-self-administration-client", "ADMIN");
+        removeScopesFromClient("addon-administration-client", "GET", "POST", "PUT", "PATCH", "DELETE");
+        addScopesToClient("addon-administration-client", "ADMIN");
     }
 
-    private List<Long> clientIds(Connection connection) throws SQLException {
-        List<Long> result = new ArrayList<>();
-        try (PreparedStatement statement = connection.prepareStatement("select internal_id from osiam_client")) {
-            ResultSet resultSet = statement.executeQuery();
-            while (resultSet.next()) {
-                long clientId = resultSet.getLong(1);
-                result.add(clientId);
-            }
-            return result;
-        }
-    }
-
-    private void removeScopeFromClient(Long clientId, String scope, Connection connection) throws SQLException {
-        if(clientId < 0) {
+    private void removeScopesFromClient(String clientId, String... scopes) throws SQLException {
+        long internalId = toInternalId(clientId);
+        if (internalId < 0) {
             return;
         }
-        try (PreparedStatement statement = connection
-                .prepareStatement("delete from osiam_client_scopes where id = ? and scope = ?")) {
-            statement.setLong(1, clientId);
-            statement.setString(2, scope);
-            statement.execute();
+        for (String scope : scopes) {
+            try (PreparedStatement statement = connection
+                    .prepareStatement("delete from osiam_client_scopes where id = ? and scope = ?")) {
+                statement.setLong(1, internalId);
+                statement.setString(2, scope);
+                statement.execute();
+            }
         }
     }
 
-    private void addScopeToClient(Long clientId, String scope, Connection connection) throws SQLException {
-        if(clientId < 0) {
+    private void addScopesToClient(String clientId, String... scopes) throws SQLException {
+        long internalId = toInternalId(clientId);
+        if (internalId < 0) {
             return;
         }
-        try (PreparedStatement statement = connection
-                .prepareStatement("insert into osiam_client_scopes (id, scope) values (?, ?)")) {
-            statement.setLong(1, clientId);
-            statement.setString(2, scope);
-            statement.execute();
+        for (String scope : scopes) {
+            try (PreparedStatement statement = connection
+                    .prepareStatement("insert into osiam_client_scopes (id, scope) values (?, ?)")) {
+                statement.setLong(1, internalId);
+                statement.setString(2, scope);
+                statement.execute();
+            }
         }
     }
 
-    private long toInternalId(String clientId, Connection connection) {
-        try (PreparedStatement statement = connection
-                .prepareStatement("select internal_id from osiam_client where id = ?")) {
-            statement.setString(1, clientId);
-            ResultSet resultSet = statement.executeQuery();
-            if (resultSet.next()) {
-                return resultSet.getLong(1);
+    private long toInternalId(String clientId) {
+        if (!internalIds.containsKey(clientId)) {
+            try (PreparedStatement statement = connection
+                    .prepareStatement("select internal_id from osiam_client where id = ?")) {
+                statement.setString(1, clientId);
+                ResultSet resultSet = statement.executeQuery();
+                if (resultSet.next()) {
+                    internalIds.put(clientId, resultSet.getLong(1));
+                } else {
+                    internalIds.put(clientId, -1L);
+                }
+            } catch (SQLException e) {
+                internalIds.put(clientId, -1L);
             }
-            return -1;
-        } catch (SQLException e) {
-            return -1;
         }
+        return internalIds.get(clientId);
     }
 }
