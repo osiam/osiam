03e48c2a81304983312e3ad49e9b43f0491bcf3a
Thomas Krille <t.krille@tarent.de>
2016-01-03 23:58:33 +0100

    Permit access to `/error` by everyone
    The Spring Boot error handler should be accessible without
    authentication.
    This way one can consume error messages from failed logins.
    Fixes an issue with deployment on Tomcat where some failed logins return
    a redirect to the login page instead of the correct JSON response.

--- src/main/java/org/osiam/configuration/WebApplicationSecurity.java
+++ src/main/java/org/osiam/configuration/WebApplicationSecurity.java
@@ -71,7 +71,7 @@ public class WebApplicationSecurity extends WebSecurityConfigurerAdapter {
         );
 
         http.authorizeRequests()
-                .antMatchers("/login")
+                .antMatchers("/login", "/error")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
